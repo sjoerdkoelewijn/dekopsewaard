0434caba4b991ff1746a3b0c36ddb971187b359b
Sjoerd <hello@sjoerdkoelewijn.com>
2021-01-21 11:18:25 +0100

    Revert "Revert "changed nested p tags in sidebar text""
    This reverts commit a020f51528080a8fc369eda7092be20f1d2496fe.

--- src/templates/page.js
+++ src/templates/page.js
@@ -236,7 +236,7 @@ const Page = ({data}) => {
                   
                     <aside className={styles.sidebar}>
 
-                      <p className="sidebar_text" dangerouslySetInnerHTML={{
+                      <div className="sidebar_text" dangerouslySetInnerHTML={{
                         __html: data.wordPress.pageBy.page_type.text,
                         }}
                       />
